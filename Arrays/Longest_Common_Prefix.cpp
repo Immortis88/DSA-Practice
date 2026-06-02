1403e3cba6277037c1d48d801a8a0bd520b8c2e7
Bharat Dhakad <bharatdhakad2006@gmail.com>
2026-06-02 23:31:59 +0530

    feat: LC 2 Longest Common Prefix (Horizontal scanning) - Brute force

---
+++ Arrays/Longest_Common_Prefix.cpp
@@ -0,0 +1,30 @@
+/* LC 14 : Longest Common Prefix
+
+NOTE: i got the logic right but i didnt have knowledge reserve enough to write the library functions 
+on my own. 
+overall the probleum is okay
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.size() == 0){return "";}
+        string prefix = strs[0];
+        for(int i =0;i<strs.size();i++){
+            while(strs[i].find(prefix) !=0){
+                prefix = prefix.substr(0, prefix.length()-1); 
+            }
+        } return prefix;
+    }  
+};
+
+int main(){
+Solution sol;
+vector<string> strs = {"flower","flow","flight"};
+string result = sol.longestCommonPrefix(strs);
+cout<<result;
+    return 0;
+}
